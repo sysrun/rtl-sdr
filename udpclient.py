fd7ac33ef60be95090ed7f1bc7a8c97f15766b82
Frederik Granna <webmaster@granna.de>
2013-10-28 21:21:03 +0100

    python client for rtl_udp

---
+++ udpclient.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import socket, sys
+
+if len(sys.argv) < 3:
+	sys.exit(3)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("localhost", 6020))
+
+buf = ""
+
+mode = sys.argv[1]
+data = int(sys.argv[2])
+
+if mode == 'freq':
+	buf = buf + chr(0)
+elif mode == 'mode':
+	buf = buf + chr(1)
+else:
+	sys.exit(1)
+
+i=0
+while i < 4:
+	buf = buf + chr(data & 0xff)
+	data = data >> 8
+	i = i + 1
+
+
+s.send(buf)
+s.close()
